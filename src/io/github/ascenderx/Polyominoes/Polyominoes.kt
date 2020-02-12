eb52e3bc44c3edaf0a06046d434f1b13424954cc
James Downer <downer.james.256k@gmail.com>
2020-02-12 16:35:29 -0700

    Rename main Kt file

---
+++ src/io/github/ascenderx/Polyominoes/Polyominoes.kt
@@ -0,0 +1,19 @@
+package io.github.ascenderx.Polyominoes
+
+import java.awt.EventQueue
+
+import io.github.ascenderx.Polyominoes.models.Drawable
+import io.github.ascenderx.Polyominoes.models.FramerateTimer
+import io.github.ascenderx.Polyominoes.models.Game
+import io.github.ascenderx.Polyominoes.models.InputHandler
+import io.github.ascenderx.Polyominoes.views.GameCanvas
+import io.github.ascenderx.Polyominoes.views.GameWindow
+
+class Polyominoes {
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      
+    }
+  }
+}
